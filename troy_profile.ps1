edf28e247f645322cc692d2963611c79c86ad17c
Troy Dai <trdai@TDI-EliteBook.redmond.corp.microsoft.com>
2013-11-21 14:18:28 -0800

    init my powershell profile

---
+++ troy_profile.ps1
@@ -0,0 +1,10 @@
+﻿$root = $env:USERPROFILE + "\SkyDrive"
+
+if (Test-Path $root) {
+    # Set up prompt
+    . "$root\Development\posh-git\profile.example.ps1"
+
+    # Sublime Text 
+    set-alias -name subl -value "$root\Software\Sublime_Text\sublime_text.exe"
+}
+
