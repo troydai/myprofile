d0e74327ac1d1c01d04365b652601d34b7dc8c8c
Troy Dai <trdai@TDI-EliteBook.redmond.corp.microsoft.com>
2013-11-22 11:11:55 -0800

    Add git alias

--- troy_profile.ps1
+++ troy_profile.ps1
@@ -6,5 +6,8 @@ if (Test-Path $root) {
 
     # Sublime Text 
     set-alias -name subl -value "$root\Software\Sublime_Text\sublime_text.exe"
+
+    # GIT
+    set-alias -name git -value "$root\Software\Git\Cmd\git.exe"
 }
 
