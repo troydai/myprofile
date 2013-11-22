1cdff7f0cf114198ca08e7d68fc64c7ef26d28a4
Troy Dai <trdai@TDI-EliteBook.redmond.corp.microsoft.com>
2013-11-22 11:20:59 -0800

    move prompt update to latter part

--- troy_profile.ps1
+++ troy_profile.ps1
@@ -1,13 +1,12 @@
 ﻿$root = $env:USERPROFILE + "\SkyDrive"
 
 if (Test-Path $root) {
-    # Set up prompt
-    . "$root\Development\posh-git\profile.example.ps1"
-
     # Sublime Text 
     set-alias -name subl -value "$root\Software\Sublime_Text\sublime_text.exe"
 
     # GIT
     set-alias -name git -value "$root\Software\Git\Cmd\git.exe"
-}
 
+    # Set up prompt
+    . "$root\Development\posh-git\profile.example.ps1"
+}
