27b746b0f9f3a4d25d32ed3b2f3b6ff05f995b19
Troy Dai <titan_dai@msn.com>
2014-03-12 22:34:38 -0700

    update

--- troy_profile.ps1
+++ troy_profile.ps1
@@ -5,7 +5,10 @@ if (Test-Path $root) {
     set-alias -name subl -value "$root\Software\Sublime_Text\sublime_text.exe"
 
     # GIT
-    set-alias -name git -value "$root\Software\Git\Cmd\git.exe"
+    # set-alias -name git -value "$root\Software\Git\Cmd\git.exe"
+
+    # Add SSH to path
+    $env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\bin"
 
     # Set up prompt
     . "$root\Development\posh-git\profile.example.ps1"
@@ -14,9 +17,12 @@ if (Test-Path $root) {
     if (Test-Path $env:VS120COMNTOOLS) {
         set-alias -name tf -value "$env:VS120COMNTOOLS..\IDE\tf.exe"
     }
+    elseif (Test-Path $env:VS110COMNTOOLS) {
+        set-alias -name tf -value "$env:VS110COMNTOOLS..\IDE\tf.exe"
+    }
 
     # Set up TFPT
-    if (Test-Path $env:TFSPowerToolDir) {
+    if (($env:TFSPowerToolDir -ne $null) -and (Test-Path $env:TFSPowerToolDir)) {
         set-alias -name tfpt -value "${env:TFSPowerToolDir}TFPT.EXE"
     }
 }
