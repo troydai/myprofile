cf4bf29f53b8efad756e43ce6dbb559feea4089c
Troy Dai <troy.dai@outlook.com>
2014-10-14 10:02:34 -0700

    Update troy_profile.ps1

--- troy_profile.ps1
+++ troy_profile.ps1
@@ -1,15 +1,15 @@
 Write-Host "Welcome, Troy!"
 
-# Sublime text
+# Add SSH to path
+$env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\bin"
+
+# Posh-Git
 $myGit = $env:USERPROFILE + "\git"
 if (Test-Path "$myGit\posh-git\profile.example.ps1")
 {
     . $myGit\posh-git\profile.example.ps1
 }
 
-# Add SSH to path
-$env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\bin"
-
-# Program files
+# Sublime Text 2
 $sublimeTextExec = $env:ProgramFiles + "\Sublime Text 2\sublime_text.exe"
 set-alias subl $sublimeTextExec
