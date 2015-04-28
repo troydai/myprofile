9c7879ae7a066e03f53f268ea3c5e8ae531d1ec2
Troy Dai <troy.dai@outlook.com>
2015-04-28 10:02:06 -0700

    Submodule posh-git

--- troy_profile.ps1
+++ troy_profile.ps1
@@ -1,15 +1,41 @@
 Write-Host "Welcome, Troy!"
 
+$dp0 = Split-Path $MyInvocation.MyCommand.Path
+
+# Sublime
+$sublimeTextExec = $env:ProgramFiles + "\Sublime Text 3\sublime_text.exe"
+if (-not (Test-Path $sublimeTextExec))
+{
+    $sublimeTextExec = $env:ProgramFiles + "\Sublime Text 2\sublime_text.exe"
+}
+
+set-alias subl $sublimeTextExec
+
 # Add SSH to path
 $env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\bin"
 
 # Posh-Git
-$myGit = $env:USERPROFILE + "\git"
-if (Test-Path "$myGit\posh-git\profile.example.ps1")
-{
-    . $myGit\posh-git\profile.example.ps1
+
+# Load posh-git module from current directory
+Import-Module $dp0\posh-git
+
+# Set up a simple prompt, adding the git prompt parts inside git repos
+function global:prompt {
+    $realLASTEXITCODE = $LASTEXITCODE
+
+    # Reset color, which can be messed up by Enable-GitColors
+    $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+
+    Write-Host($pwd.ProviderPath) -nonewline
+
+    Write-VcsStatus
+
+    $global:LASTEXITCODE = $realLASTEXITCODE
+
+    Write-Host ""
+    return "$ "
 }
 
-# Sublime Text 2
-$sublimeTextExec = $env:ProgramFiles + "\Sublime Text 2\sublime_text.exe"
-set-alias subl $sublimeTextExec
+Enable-GitColors
+
+Start-SshAgent -Quiet
