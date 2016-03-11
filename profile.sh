3ca8f6656755022b271b74574b0f6e9dc6702957
Troy Dai <troy.dai@outlook.com>
2016-03-11 10:20:38 -0800

    dotnet-cli issue 531 was fixed (by me)

--- profile.sh
+++ profile.sh
@@ -53,17 +53,3 @@ else
         echo "Missing vim bin. Looked at $vim_path. and /usr/bin/vim"
     fi
 fi
-
-# mock GAC
-# set the DOTNET_REFERENCE_ASSEMBLIES_PATH to mono reference assemblies folder
-# https://github.com/dotnet/cli/issues/531
-if [ -z "$DOTNET_REFERENCE_ASSEMBLIES_PATH" ]; then
-  if [ $(uname) == Darwin ] && [ -d "/Library/Frameworks/Mono.framework/Versions/Current/lib/mono/xbuild-frameworks" ]; then
-    export DOTNET_REFERENCE_ASSEMBLIES_PATH="/Library/Frameworks/Mono.framework/Versions/Current/lib/mono/xbuild-frameworks"
-  elif [ -d "/usr/local/lib/mono/xbuild-frameworks" ]; then
-    export DOTNET_REFERENCE_ASSEMBLIES_PATH="/usr/local/lib/mono/xbuild-frameworks"
-  elif [ -d "/usr/lib/mono/xbuild-frameworks" ]; then
-    export DOTNET_REFERENCE_ASSEMBLIES_PATH="/usr/lib/mono/xbuild-frameworks"
-  fi
-fi
-
