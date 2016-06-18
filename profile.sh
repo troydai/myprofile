e560f5abf7c4d7cb1c40b9f1b54e2aa2bae24b6e
Troy Dai <troy.dai@outlook.com>
2016-06-18 16:13:29 -0700

    Update profile of *nix

--- profile.sh
+++ profile.sh
@@ -14,17 +14,9 @@ if [ -f $bash_git_prompt_path ]; then
 else
     echo "Missing Bash Git Prompt. Try restore the submodules."
 fi
-local_dnvm_path="$modules/aspnet-home/dnvm.sh"
-if [ -f $local_dnvm_path ]; then
-    source $local_dnvm_path
-else
-    echo "Missing DNVM. Try restore the submodules."
-fi
-
-export DNX_UNSTABLE_FEED="https://www.myget.org/F/aspnetcidev/api/v2"
 
 code () {
-    VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $*
+  VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $*
 }
 
 home () {
@@ -38,27 +30,10 @@ go-profile () {
     cd $dp0
 }
 
-dnx-clean-packages () {
+nuke-packages () {
     $dp0/modules/k.toolkit/clean-packages.py
 }
 
-dnx-clean-runtimes () {
+nuke-runtimes () {
     $dp0/modules/k.toolkit/clean-runtimes.py
 }
-
-update-dotnet () {
-    mkdir -p /tmp/dotnet_install
-    curl -o /tmp/dotnet_install/install.sh "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/install.sh" -s
-    chmod +x /tmp/dotnet_install/install.sh
-    /tmp/dotnet_install/install.sh -c beta
-}
-
-# vim
-vim_path="/usr/local/Cellar/vim/7.4.979/bin"
-if [ -d $vim_path ]; then
-    PATH=$vim_path:${PATH}
-else
-    if [ ! -f /usr/bin/vim ]; then
-        echo "Missing vim bin. Looked at $vim_path. and /usr/bin/vim"
-    fi
-fi
