b1dc1f3b305711796c7744fdcccd109d297845a2
Troy Dai <troy.dai@outlook.com>
2016-06-19 16:19:03 -0700

    update profile.sh

--- profile.sh
+++ profile.sh
@@ -31,7 +31,7 @@ go-profile () {
 }
 
 go-dotnet () {
-  cd $( dirname `type dotnet | cut -d " " -f 3` )
+  cd $( dirname `type -p dotnet` )
 }
 
 go-nuget() {
@@ -39,9 +39,39 @@ go-nuget() {
 }
 
 nuke-packages () {
-    $dp0/modules/k.toolkit/clean-packages.py
+  $dp0/modules/k.toolkit/clean-packages.py
 }
 
-nuke-runtimes () {
-    $dp0/modules/k.toolkit/clean-runtimes.py
+nuke-cli () {
+  type dotnet > /dev/null 2>&1 || { echo ".NET CLI is not found"; return; }
+
+  dotnet_folder=$( dirname `type -p dotnet`)
+  echo "Removing .NET CLI at $dotnet_folder"
+
+  rm -rf $dotnet_folder
+}
+
+get-cli () {
+  branch="1.0.0-preview2"
+
+  echo "Getting latest .NET CLI from branch $branch"
+  install_script_url="https://raw.githubusercontent.com/dotnet/cli/rel/$branch/scripts/obtain/dotnet-install.sh"
+  curl -L $install_script_url | bash
+  dotnet --info || { echo "Failed to install .NET CLI"; }
+}
+
+list-sdks () {
+  type dotnet > /dev/null 2>&1 || { echo ".NET CLI is not found"; return; }
+
+  dotnet_folder=$( dirname `type -p dotnet`)
+
+  ls $dotnet_folder/sdk
+}
+
+list-shared-runtimes () {
+  type dotnet > /dev/null 2>&1 || { echo ".NET CLI is not found"; return; }
+
+  dotnet_folder=$( dirname `type -p dotnet`)
+
+  ls $dotnet_folder/shared/Microsoft.NETCore.App/
 }
